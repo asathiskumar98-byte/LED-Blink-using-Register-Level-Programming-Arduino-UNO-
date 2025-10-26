078ab77c406e41638245d3b97879c51ca4536bb0
Sathiskumar A <asathiskumar98@gmail.com>
2025-10-26 21:20:37 +0530

    Add files via upload

---
+++ Single_LED_Interface.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+  DDRB |= (1<<DDB5);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  PORTB |= (1<<PB5);
+  delay(100);
+  PORTB &= (~(1<<PB5));
+  delay(100);  
+}
